b93a1af14a6279484cfdafa7c95d9569903f5f7a
Jingkai Tang <jingkaitang@qq.com>
2017-04-15 16:08:49 +0800

    update: url

--- cernet-login.sh
+++ cernet-login.sh
@@ -4,6 +4,8 @@
 username="your_username"
 password="your_password"
 
-url="http://10.6.8.2/cgi-bin/srun_portal?ac_id=1&action=login&type=1&username=$username&password=$password" 
-wget -q -O /dev/null $url
+url="http://10.6.8.2:904/include/auth_action.php"
+data="action=login&username=$username&password=$password&ac_id=1&user_ip=&nas_ip=&user_mac=&save_me=1&ajax=1Name"
+
+curl -X POST -d $data $url
 
